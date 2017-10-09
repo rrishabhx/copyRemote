fd817caa585b0b8a8906d01b5af13751190fa13a
Risabh Raj <risabhRizz@users.noreply.github.com>
2017-10-09 11:09:43 +0530

    Improve script prompts by adding colors

--- rzcopyremote.sh
+++ rzcopyremote.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 clear
-echo -e "\e[1;34m*****Remote Copying process about to begin*****\e[0m\n"
-sleep 2
+echo -e "\e[1;34m+++++++++++++++++++++++++++++++++++++++++++++++++++++\e[0m"
+echo -e "\e[1;34m -----> REMOTE COPYING PROCESS ABOUT TO BEGIN ----->\e[0m"
+echo -e "\e[1;34m+++++++++++++++++++++++++++++++++++++++++++++++++++++\e[0m\n"
+sleep 1
 #IP sheet should be provided as argument 1.
 if [ ! -f "$1" ];then
-  echo -e "IP sheet doesn't exist. Aborting process.....\n"
-  sleep 2
+  echo -e "\e[1;31mIP sheet doesn't exist. Aborting process.....\e[0m\n"
+  sleep 1
   exit 1
 else
-  echo -e "IP sheet exists. Continuing process.....\n"
-  sleep 2
+  echo -e "\e[1;32mIP sheet exists. Continuing process.....\e[0m\n"
+  sleep 1
 fi
 
 
 #Source file location should be given as argument 2.
 if [ -f "$2" ] || [ -d "$2" ];then
-  echo -e "Input file/folder exists. Continuing process.....\n"
-  sleep 2
+  echo -e "\e[1;32mInput file/folder exists. Continuing process.....\e[0m\n"
+  sleep 1
 
 else
-  echo -e "Input file/folder doesn't exist. Aborting process.....\n"
-  sleep 2
+  echo -e "\e[1;31mInput file/folder doesn't exist. Aborting process.....\e[0m\n"
+  sleep 1
   exit 1
 fi
 
@@ -28,18 +30,23 @@ source_file="$2"
 export source_file
 
 #Give destination Copy path
-read -p "Enter the Destination location (Directory path): " dest
+echo -en "\e[1;34mEnter the Destination location (Directory path): \e[0m" 
+read dest
 export dest
 
 #Copying data PAN India
-echo -e "***Copying $2 to the remote servers***\n"
-sleep 2
+echo -e "\n***Copying $2 to the remote servers***\n"
+sleep 1
 N=$(wc -l < "$1" )
+echo -en "\n\e[1;34mEnter Username: \e[0m"
+read username
+echo -en "\e[1;34mEnter Password: \e[0m"
+read -s password
+echo
 for ((i=1;i<=$N;i++))
 do
   ipc=$(awk "NR==$i" "$1")
   echo -e "\n\e[1;32mBlade IP = $ipc\e[0m"
   export ipc
-  $(pwd)/scp_rzcopyremote.tcl
+  $(pwd)/scp_rzcopyremote.tcl $username $password
 done
-
