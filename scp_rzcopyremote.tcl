71c4deda13fadcf9247ff650d4a1ac2a47cc5e99
Risabh Raj <risabhRizz@users.noreply.github.com>
2017-10-09 10:54:10 +0530

    Update expect script get username and password input.
    Expect script now gets username and password from main script as arguments.

--- scp_rzcopyremote.tcl
+++ scp_rzcopyremote.tcl
@@ -1,10 +1,8 @@
 #!/usr/bin/expect
-#Replace username with your username
-spawn scp -o StrictHostKeyChecking=no -r $::env(source_file) USERNAME@$::env(ipc):$::env(dest)
+set username [lindex $argv 0]
+set password [lindex $argv 1]
+spawn scp -o StrictHostKeyChecking=no -r $::env(source_file) $username@$::env(ipc):$::env(dest)
 expect "assword: "
-#Enter password for the servers inside double quotes and  before '\r'  
-#eg: send "mypassword\r"
-#Write password below
-send "----\r" 
+send "$password\r" 
 interact
 send_user "\n>>>>>>>>>>>>>>>>>>>>>>>>> Copied successfully to Remote server $::env(ipc) <<<<<<<<<<<<<<<<<<<<<<<<<\n"
